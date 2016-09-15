7bb9bd57d6e30310e7b875fb8fedcbbb1d29de75
lorwal <lorwal@me.com>
2016-09-15 02:11:47 +0200

    adjust test cfactor

--- tests/testthat/test-factor_creation.R
+++ tests/testthat/test-factor_creation.R
@@ -3,7 +3,6 @@ context("cfactor")
 case1 <- cfactor(rep("x", 5))
 case2 <- cfactor(letters, labels = "letter")
 case3 <- cfactor(sample(letters, size = 400, replace = TRUE), levels = letters)
-case4 <- cfactor(sample(letters, size = 400, replace = TRUE))
 
 test_that("cfactor returns a factor", {
   expect_output(str(case1), "Factor")
@@ -15,8 +14,7 @@ test_that("cfactor returns expected levels", {
   expect_equal(levels(case1), "x")
   expect_equal(levels(case2), paste("letter", 1:26, sep = "")) # is this really desired
   expect_equal(levels(case3), letters)
-  expect_equal(levels(case4), letters)
-  
+
 })
 
 # connector esape hatch
