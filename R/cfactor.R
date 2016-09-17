22e4a7a4118f2eaaba2e868a96ed8a3fc6a9a8ee
lorenzwalthert <lorenz.walthert@icloud.com>
2016-09-17 23:47:26 +0200

    update documentaion
    ntroduce @inheritParams from base factor for x, levels and labels. Added details section and require numbers in every string of x to apply the level detection algorithm

--- R/cfactor.R
+++ R/cfactor.R
@@ -1,16 +1,14 @@
 #' Factor
 #'
-#' A wrapper for factor with enhanced control.
-#'
-#' @param x A vector of data, usually taking a small number of distinct values.
-#' @param levels An optional vector of the values (as character strings) that x might have taken. The default is the unique set
-#' of values taken by as.character(x), sorted into increasing order of x. Note that this set can be specified as smaller than
-#' sort(unique(x)).
-#' @param labels Either an optional character vector of labels for the levels (in the same order as levels after removing those in exclude),
-#' or a character string of length 1.
-#' @param sep A character vector giving all strings that are used to separate the lower boundary of a range from the upper boundary.
-#' @return An object of class "factor" which has a set of integer codes the length of x with a "levels" attribute of mode character
-#' and unique (!anyDuplicated(.)) entries. If argument ordered is true (or ordered() is used) the result has class c("ordered", "factor").
+#' A wrapper for \code{factor} with enhanced control.
+#' @inheritParams base::factor
+#' @param sep A character vector giving all strings that are used to separate the lower numerical boundary of a range from the upper 
+#'  numerical boundary within \code{x}. 
+#' @return An object of class "factor" which has a set of integer codes the length of \code{x} with a "levels" attribute of mode character
+#'  and unique (!anyDuplicated(.)) entries. If argument ordered is true (or ordered() is used) the result has class c("ordered", "factor").
+#' @details \code{cfactor} wraps \code{\link{factor}} but provides enhanced control. \cr
+#'  The order of the levels is determined by sorting the numerical values preceeding the separators indicated in \code{sep}. If 
+#'  \code{sep} is set to \code{NULL} or if not every value of \code{x} contains numbers, the same ordering as in \code{factor} is applied. 
 #' @examples cfactor(c("a", "c", "b", "c", "d"))
 #' @export
 cfactor <- function(x, levels, labels = levels, exclude = NA, ordered = is.ordered(x), nmax = NA, sep = c("-", "to")) {
@@ -18,8 +16,8 @@ cfactor <- function(x, levels, labels = levels, exclude = NA, ordered = is.order
   `%w/o%` <- function(x, y) x[!x %in% y] # opposite of %in%
 
   if(missing(levels)){ # detect factor levels if not given
-    uniq_x <- unique(x)
-    has_numbers <- any(grepl("[[:digit:]]", uniq_x))
+    uniq_x <- unique(na.omit(x))
+    has_numbers <- all(grepl("[[:digit:]]", uniq_x))
     if(!is.null(sep) && has_numbers){ # use regular expression algorithm
       sep.ready <- paste0(sep, collapse = "|")
       sep <- regexec(sep.ready, uniq_x)
