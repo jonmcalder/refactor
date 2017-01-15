f68ef8dccf4a96ea4d5ff87014a434c9f519b2d7
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-01-15 23:18:46 +0100

    removed ... argument of append to make it less complex (and more similar to base::append. Require both x and values arguments to be of type factor, not just one.

--- R/append.R
+++ R/append.R
@@ -1,13 +1,15 @@
 #' Vector Merging
 #' 
 #' @inheritParams base::append
-#' @param ... further arguments to be passed to \code{\link{cfactor}}.
 #' @details A refined version of \code{\link[base]{append}} that allows for
 #'   factor appending.
 #' @return 
-#'   If either \code{x} or \code{values} are of class \code{factor}, 
+#'   If \code{x} and \code{values} are of class \code{factor}, 
 #'   the function returns an object of class \code{factor} whereas levels are 
-#'   determined using \code{\link{cfactor}} internally. Otherwise, the base 
+#'   determined using \code{\link{cfactor}} internally. Levels are only
+#'   preserved if both elements have the same levels. In addition, ordering
+#'   is only preserved if the order is also identical.
+#'   Otherwise, the base 
 #'   method is called directly.
 #' @examples 
 #' f <- factor(c('c','a','a',NA,'b','a'), levels= c('a','b','c'))
@@ -16,19 +18,34 @@
 #' refactor::append(f, g) # probably what you want
 #' @export
 
-append <- function (x, values, after = length(x), ...) {
-  was_factor <- FALSE
+append <- function (x, values, after = length(x)) {
+  count <- 0
+  
   if(is.factor(x)) {
-    was_factor <- TRUE 
+    lvls_x <- levels(x) 
+    is_ordered_x <- is.ordered(x)
     x <- as.character(x)
+    count <- count + 1 #
   }
+  
   if(is.factor(values)) {
-    was_factor <- TRUE 
+    lvls_values <- levels(values)
+    is_ordered_values <- is.ordered(values)
     values <- as.character(values)
+    count <- count + 1
   }
+
   out <- base::append(x, values, after)
-  if(was_factor) {
-    cfactor(out, ...)
+  
+  if(count == 2) { # returns factor if both were factors
+    # if levels are the same, preserve them
+    if (identical(lvls_x, lvls_values)) {
+      
+    # if order of the levels is the same, preserve it.
+    cfactor(out, levels = lvls_x, ordered = all(is_ordered_x, is_ordered_values))
+    } else {
+      cfactor(out)
+    }
   } else {
     out
   }
