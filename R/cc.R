a701e7e4bd65484ba8684a222a54aef3e5cff05f
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-01-19 22:07:25 +0100

    simplify cc, update documentation with examples and improve tests.

--- R/cc.R
+++ R/cc.R
@@ -1,45 +1,66 @@
-#' Combine Values into an (ordered) Factor
+#' Enhanced control for combining values
 #' 
 #' @inheritParams base::c
-#' @details Note that the order of the levels is internally determined by
-#' \code{\link{cfactor}} and thus its ordering scheme. Refer to the section 
-#' 'Details' in the \code{\link{cfactor}} documentation.
+#' @details This function returns the same as base \code{\link{c}}, but if all
+#'  elements of \code{...} are of class factor or ordered, it returns the lowest
+#'  common denominator, i.e. an object of class ordered or factor. 
+#'  Ordering of the levels (also for unordered factors) is only preserved if it 
+#'  is identical accross all element in \code{...}. Otherwise, it is determined 
+#'  by \code{\link{cfactor}}, which is called internarlly. Refer to the section 
+#' 'Details' in the \code{\link{cfactor}} documentation for further details.
 #' @examples
 #' char1 <- as.factor(sample(letters, 5))
 #' char2 <- as.factor(sample(letters, 5))
-#' cc(char1, char2)
+#' cc(char1, char2) # probably more reasonable than
+#' c(char1, char2) 
+#' \dontrun{
+#' ## no warning since levels identical
+#' a_to_d <- cfactor(letters[1:2], levels = letters[1:5])
+#' b_to_d <- cfactor(letters[1:5])
+#' cc(a_to_d, b_to_d)
+#' ## warning for differing order of levels
+#' a_to_d <- cfactor(letters[1:2], levels = letters[1:5])
+#' d_to_a <- cfactor(letters[1:2], levels = rev(letters[1:5]))
+#' cc(a_to_d, d_to_a)
+#' 
+#' ## warning for different levels
+#' some_levels <- cfactor(letters[1:2], levels = letters[1:5])
+#' all_levels <- cfactor(letters[1:2], levels = letters)
+#' cc(all_levels, some_levels, some_levels)
+#' }
 #' @export
 cc <- function(..., recursive = FALSE){
+  
   dots = list(...)
-
   dots_chars <- lapply(dots, FUN = as.character)
   
+  levels <- lapply(dots, function(x) levels(x))
+  
+  levels_identical <- Reduce(identical, levels)
+  levels_same_set <- Reduce(setequal, levels)
 ##  ............................................................................
-##  test whether objects inherit from ordered
+##  test whether objects inherit from ordered or factor
   dots_ordered <- vapply(dots, function(x) inherits(x, "ordered"), logical(1))
-  if (all(dots_ordered)) {
-    # ok, its all ordered. Keep as ordered if all levels and their ordering is 
-    # the same
-    levels <- lapply(dots, function(x) levels(x))
-    levels_identical <- Reduce(identical, levels)
-    if (levels_identical) {
-      return(cfactor(unlist(dots_chars), levels[[1]], ordered = TRUE))
-    } else {
-      warning("ordering not preserved since not all levels identical", 
+  dots_factor <-  vapply(dots, function(x) inherits(x, "factor"), logical(1))
+  
+  if (all(dots_factor)) { # if all elements are at least factors
+    if (levels_same_set) {
+      if (levels_identical) {
+        return(cfactor(unlist(dots_chars), levels[[1]], ordered = all(dots_ordered)))
+      } else if (!levels_identical) {
+        warning("ordering not preserved since ordering of levels not identical", 
+                call. = FALSE)
+        return(cfactor(unlist(dots_chars), ordered = FALSE))  
+      }
+    } else if (!levels_same_set){
+      warning("ordering and levels not preserved since levels not identical", 
               call. = FALSE)
       return(cfactor(unlist(dots_chars), ordered = FALSE))
     }
-  }
-  
+    
+  } else {
 ##  ............................................................................
-##  test whether objects inherit from factor
-  dots_factor <- vapply(dots, function(x) inherits(x, "factor"), logical(1))
-  if (all(dots_factor)) {
-    # ok, its all factorial
-    return(cfactor(unlist(dots_chars), ordered = FALSE))
+##  it's neither factorial nor ordered, so we simply call base::c
+  return(c(unlist(dots)))
   }
-  
-##  ............................................................................
-##  it's not factorial nor ordered, so we simply call base::c
-  return(c(unlist(dots_chars)))
 }
--- tests/testthat/test-cc.R
+++ tests/testthat/test-cc.R
@@ -38,14 +38,49 @@ test_that("cc returns an ordered factor", {
   expect_factor(cc(orderednumfactor2, orderednumfactor2))
 })
 
+### .. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+newlevels <- cfactor(orderedfactor1, levels = rev(levels(orderedfactor1)))
+newlevels_unordered1 <- cfactor(orderedfactor1, 
+                               levels = rev(levels(orderedfactor1)), 
+                               ordered = FALSE)
+newlevels_unordered2 <- cfactor(orderedfactor1, 
+                               levels = levels(orderedfactor1), 
+                               ordered = FALSE)
+
+test_that("cc returns warnings", {
+  # for ordered factors
+  expect_warning(cc(orderedfactor1, orderednumfactor1), 
+                 "ordering and levels not preserved since levels not identical")
+  expect_identical(class(cc(orderedfactor1, orderednumfactor1)), 
+                   "factor")
+  expect_warning(cc(orderedfactor1, newlevels), 
+                 "ordering not preserved since ordering of levels not identical")
+  expect_identical(class(cc(orderedfactor1, newlevels)), 
+                   "factor")
+  
+  
+  # for ordered factors
+  expect_warning(cc(newlevels_unordered2, newlevels_unordered1), 
+                 "ordering not preserved since ordering of levels not identical")
+  expect_identical(class(cc(newlevels_unordered2, newlevels_unordered1)), 
+                   "factor")
+  
+  # for mixed input
+  expect_warning(cc(newlevels_unordered2, newlevels),
+                 "ordering not preserved since ordering of levels not identical")
+
+})
+
 context("cc mixed input")
 char1 <- sample(letters)
 num1 <- 1:10
 boo1 <- c(TRUE, FALSE)
-test_that("cc returns a character vector if input is mixed (factor and character", {
-  expect_character(cc(char1, factor1 ))
-  expect_character(cc(num1, factor1 ))
-  expect_character(cc(boo1, factor1 ))
-    
+test_that("cc returns same as c if not all input elements are at least factors", {
+  # two different classes
+  expect_identical(cc(char1, factor1), c(char1, factor1))
+  expect_identical(cc(char1, num1), c(char1, num1))
+  expect_identical(cc(factor1, num1), c(factor1, num1))
+  # same class
+  expect_identical(cc(char1, char1), c(char1, char1))
+  expect_identical(cc(boo1, boo1), c(boo1, boo1))
 })
-  
